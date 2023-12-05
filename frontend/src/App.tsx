40e2ef77b719b27e50c8d5a301e12b0d3c18efce
Jarvx200 <jarvx404@gmail.com>
2023-12-05 23:26:13 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='dark:bg-dark-bg w-[100vw] h-[100vh]'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
