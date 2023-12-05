69d3ca08646806a7150f57c57f1660895d13b595
Jarvx200 <jarvx404@gmail.com>
2023-12-06 01:25:39 +0200

    fixed my zoom mistake and added some media

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -5,24 +5,24 @@ import { MdEmail } from "react-icons/md";
 
 const ProfileCard = () => {
     return(<>
-        <div className="w-[40vw] p-6 bg-brown flex md:flex-row flex-col items-center justify-center rounded-md ">
-            <div className="w-[50%] h-auto flex flex-col justify-center mr-1">
+        <div className="md:w-[40vw] w-[90%] p-8 bg-brown flex md:flex-row flex-col items-center justify-center rounded-md  ">
+            <div className="md:w-[50%] w-[100%] h-auto flex flex-col justify-center mr-1 md:mb-0 mb-5">
                 <img className="w-[100%] h-auto bg-cover rounded-lg mb-4" src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/a325e722353999.5631184951d59.jpg"></img>
                 <ul className="w-[100%] pl-2 pr-2 flex flex-row items-center justify-center">
-                    <li className="ml-3 mr-3"><a href="" className="text-2xl dark:text-dark-fg text-light-fg hover:text-purple transition-all"><FaGithub></FaGithub></a></li>
-                    <li className="ml-3 mr-3"><a href="" className="text-2xl dark:text-dark-fg text-light-fg hover:text-blue transition-all"><FaXTwitter></FaXTwitter></a></li>
-                    <li className="ml-3 mr-3"><a href="" className="text-2xl dark:text-dark-fg text-light-fg hover:text-yellow transition-all"><MdEmail></MdEmail></a></li>
-                    <li className="ml-3 mr-3"><a href="" className="text-2xl dark:text-dark-fg text-light-fg hover:text-green transition-all"><FaSignature></FaSignature></a></li>
+                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="https://github.com/Jarvx200" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:text-purple transition-all"><FaGithub></FaGithub></a></li>
+                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="https://twitter.com/Jarvx404" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:text-blue transition-all"><FaXTwitter></FaXTwitter></a></li>
+                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:text-yellow transition-all"><MdEmail></MdEmail></a></li>
+                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:text-green transition-all"><FaSignature></FaSignature></a></li>
                 </ul>
             </div>
-            <div className="w-[50%] h-auto flex flex-col items-center ml-1 dark:text-dark-fg text-light-fg">
-                    <h1>jarvx<span className="text-purple font-semibold">@</span>homepage</h1>
+            <div className="md:w-[50%] w-[100%] h-auto flex flex-col items-center ml-1 dark:text-dark-fg text-light-fg">
+                    <h1 className="text-3xl ">jarvx<span className="text-purple font-semibold">@</span>homepage</h1>
                     <p>- - - - - -</p>
-                    <p><span className="font-bold dark:text-dark-fg text-light-fg">Name:</span> Bigu Cezar</p>
-                    <p><span className="font-bold dark:text-dark-fg text-light-fg">Location:</span> Romania</p>
-                    <p><span className="font-bold dark:text-dark-fg text-light-fg">Uptime:</span> 16 yrs</p>
-                    <p><span className="font-bold dark:text-dark-fg text-light-fg">Pronouns:</span> he / him</p>
-                    <p><span className="font-bold dark:text-dark-fg text-light-fg">Stack:</span> MERN</p>
+                    <p className=" md:text-2xl text-xl"><span className="font-bold text-2xl  text-purple">Name:</span> Bigu Cezar</p>
+                    <p className=" md:text-2xl text-xl"><span className="font-bold text-2xl  text-blue">Location:</span> Romania</p>
+                    <p className=" md:text-2xl text-xl"><span className="font-bold text-2xl  text-yellow">Uptime:</span> 16 yrs</p>
+                    <p className=" md:text-2xl text-xl"><span className="font-bold text-2xl  text-blue">Pronouns:</span> he / him</p>
+                    <p className=" md:text-2xl text-xl"><span className="font-bold text-2xl  text-green">Stack:</span> MERN</p>
             </div>
         </div>
     </>)
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,10 +6,10 @@ const HomePage = () => {
     return(<>
         <div className="w-full h-[100vh] bg-cover flex flex-col items-center p-5 dark:bg-dark-bg bg-light-bg">
             <Navbar></Navbar>
-            <h1 className="text-5xl dark:text-dark-fg text-light-fg font-semibold mt-7">Bigu Cezar</h1>
-            <h2 className="text-[2.2vw]  text-aqua font-semibold mt-2 mb-5">(jarvx)</h2>
+            <h1 className="md:text-8xl text-6xl dark:text-dark-fg text-light-fg font-semibold mt-12">Bigu Cezar</h1>
+            <h2 className="md:text-5xl text-3xl  text-aqua font-semibold mt-2 mb-10">(jarvx)</h2>
 
-            <p className="text-purple font-semibold text-sm bg-brown p-0.5 pl-1 pr-1 rounded-md mb-2">Full Stack Web Developer / Foss && Linux Enthusiast </p>
+            <p className="text-purple font-semibold md:show hidden text-xl  bg-brown p-0.5 pl-1 pr-1 rounded-md mb-2">Full Stack Web Developer / Foss && Linux Enthusiast </p>
             
             <ProfileCard></ProfileCard>
         </div> 
