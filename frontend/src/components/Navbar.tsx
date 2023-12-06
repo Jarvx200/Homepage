b35506f6c99b053462fc06a38a49d36121a9a3d4
Jarvx200 <jarvx404@gmail.com>
2023-12-06 12:00:15 +0200

    temp active page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
 
     const [activePage, setActivePage] = useState("home");
+
+    useEffect(()=>{
+        setActivePage('home')
+    })
 
     return(
         <>
