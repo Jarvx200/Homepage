b17375933eeb448c923a9bc619c601144518598a
Jarvx200 <jarvx404@gmail.com>
2023-12-17 23:55:52 +0200

    fixed glare

--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -29,11 +29,11 @@ const  ProjectsPage = () => {
                 <div className="grid lg:grid-cols-5  grid-cols-1 gap-5 w-full h-full md:p-20">
                     {
                     projectsList.map((project) => (
-                        <Tilt glareEnable={true}>
+                        <Tilt  tiltMaxAngleX={3} tiltMaxAngleY={3}>
                         <div className="flex flex-col items-center animate-jump animate-once animate-duration-300 animate-delay-0 animate-ease-in-out w-full h-96 col-span-1 row-span lg:row-span-3 dark:bg-dark-brown bg-light-brown lg:p-0 rounded-xl">
                             <img src={project?.photoUrl} className="object-cover w-full h-full rounded-tl-xl rounded-tr-xl hover:cursor-pointer hover:opacity-50" onClick={()=>{window.open(project?.url)}}/>
                             <div className="w-full h-full flex flex-col items-center justify-start p-3">
-                                <h1 className={`text dark:text-dark-fg text-light-fg  text-3xl font-semibold mt-5`}> {project?.name} </h1>
+                                <h1 className={`text dark:text-dark-purple text-light-purple  text-3xl font-semibold mt-5`}> {project?.name} </h1>
                                 <h2 className="text dark:text-dark-fg text-light-fg  text-md font-normal mt-2 m-l2 text-center"> {project?.description} </h2>
                             </div>
                         </div>
