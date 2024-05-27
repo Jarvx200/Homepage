a38271e0eb1505c357a3f69142bcfa02fbdf0b8f
Jarvx200 <jarvx404@gmail.com>
2024-05-28 00:49:58 +0300

    added linkedin

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-import { FaGithub} from "react-icons/fa";
+import { FaGithub, FaLinkedin} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaSignature } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
@@ -28,7 +28,7 @@ const ProfileCard = () => {
                     <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="https://github.com/Jarvx200" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:dark:text-dark-purple hover:text-light-purple transition-all"><FaGithub></FaGithub></a></li>
                     <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="https://twitter.com/Jarvx404" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:dark:text-dark-blue hover:text-light-blue  transition-all"><FaXTwitter></FaXTwitter></a></li>
                     <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="mailto:cezarbigu404@gmail.com" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:dark:text-dark-yellow transition-all hover:text-light-yellow "><MdEmail></MdEmail></a></li>
-                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:dark:text-dark-aqua hover:text-light-aqua   transition-all"><FaSignature></FaSignature></a></li>
+                    <li className="ml-6 mr-6 mt-3 md:mb-0 mb-4"><a href="https://www.linkedin.com/in/bigu-cezar-9769112a0/" className="font-bold text-4xl  dark:text-dark-fg text-light-fg hover:dark:text-dark-aqua hover:text-light-aqua   transition-all"><FaLinkedin/></a></li>
                 </ul>
             </div>
             <div className="md:w-[50%] w-[100%] h-auto flex flex-col items-center ml-1 dark:text-dark-fg text-light-fg">
